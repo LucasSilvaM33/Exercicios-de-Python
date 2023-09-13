ddba1eb0cb8513434f5f6c8f9527c8bc50343db6
Lucas Martins <Lucas_Martins.33@hotmail.com>
2023-09-13 16:54:42 -0300

    primeira forma de se resolver

--- ex078.py
+++ ex078.py
@@ -2,3 +2,18 @@
 No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 '''
 
+lista = list()
+pos_maior =  []
+pos_menor = []
+for cont in range(0,5):
+    lista.append(int(input(f'Digite um valor para a posição {cont}º: ')))
+print(f'Os valores digitados foram {lista}')
+for pos, num in enumerate(lista):
+    if num == max(lista):
+        print(f'O maior valor foi {max(lista)} e ele está na posição {pos}')
+    if num == min(lista):
+        print(f'O menor valor da lista foi {min(lista)} e ele está na posição {pos}')
+print('FIM')
+
+
+
