4ef20302f629f8456d9109c21521cdb8a15a0e41
Lucas Martins <Lucas_Martins.33@hotmail.com>
2023-09-20 20:32:26 -0300

    Atividade 80

--- ex081.py
+++ ex081.py
@@ -3,4 +3,14 @@ A) Quantos números foram digitados.
 B) A lista de valores, ordenada de forma decrescente.
 C) Se o valor 5 foi digitado e está ou não na lista.'''
 
-print('Ola')
+lista = []
+escolha = ' '
+while True:
+    lista.append(int(input('Digite um número qualquer: ')))
+    escolha = str(input('Você quer continuar? [S/N]')).strip().upper()[0]
+    if escolha == 'N':
+        break
+n = len(lista)
+lista.sort(reverse=True)
+print(f'foram digitados {n} números')
+print(f'A lista em ordem decrescente: {lista}')
