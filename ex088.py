562cd637e65b347b703254bd16c0ab43eeabf0fb
Lucas Martins <Lucas_Martins.33@hotmail.com>
2023-11-11 15:16:42 -0300

    Segunda tentativa, agora com o exercício feito corretamente com a ajuda do professor

--- ex088.py
+++ ex088.py
@@ -1,26 +1,32 @@
 '''Exercício de Python 088: Faça um programa que ajude um jogador da mega sena a criar palpites.
 O progama vai perguntar quantos jogos serão geradores e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrado
  tudo em uma lista composta'''
-'''from random import randint
-dados = []
-jogo = []
 
-num = int(input('quantos jogos você quer que eu sorteie? '))
-for c in range(0, num):
-    dados.append(randint(0,60))
-    jogo.append(dados[:])
-    dados.clear()
-print(jogo, end=' ')'''
-
-'''from random import randint
-dados = []
-jogo = []
-num_jogo = int(input('Quantos jogos você quer fazer? '))
-for c in range(0, num_jogo):
-    for num_jogo in range(0,6):
-        dados.append(randint(0,60))
-        jogo.append(dados[:])
-        dados.clear()
-'''
-
-
+from random import randint
+from time import sleep
+lista = []
+jogos = []
+print('-' * 50)
+print('                    MEGA SENA                   ')
+print('-' * 50)
+escolha = int(input('Quantos jogos você quer sortear? '))
+tot = 1
+print('-' * 50)
+while escolha >= tot:
+    cont = 0
+    while True:
+        num = randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+for i, l in enumerate(jogos):
+    i += 1
+    print(f'Jogo {i}º: {l}')
+    sleep(1)
+print('BOA SORTE')
