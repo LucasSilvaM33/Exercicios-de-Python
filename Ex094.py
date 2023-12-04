acbb22843f43e93a26760001f975697505f888cc
Lucas Martins <Lucas_Martins.33@hotmail.com>
2023-12-04 17:19:44 -0300

    Adição das médias das idades na linha  18, 19, 20 e 25

--- Ex094.py
+++ Ex094.py
@@ -7,9 +7,8 @@ B) A média de idade.
 C) Uma lista com as mulheres
 D) Uma lista de pessoas com idade acima da média'''
 mulheres = []
-media = []
 dados = dict()
-tot = soma = 0
+tot = soma = maior = 0
 while True:
     dados['Nome'] = str(input('Qual o seu nome? '))
     tot += 1
@@ -18,11 +17,12 @@ while True:
         mulheres.append(dados["Nome"])
     idade = int(input('Qual a sua idade? '))
     soma += idade
-
+    media = soma/tot
     resp = str(input('Você deseja continuar (S/N) ? ')).strip().upper()[0]
     if resp not in 'Ss':
         break
 print(f'Foram cadastradas {tot} pessoas')
-print(f'A média das idades cadastradas foram de {soma/tot}')
+print(f'A média das idades cadastradas foram de {media}')
 print(f'A lista de mulheres cadastradas: {mulheres}')
+
 
